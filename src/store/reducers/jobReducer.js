c4e02f294910cc85257d3885957133ec3d2f6e61
ankit-suman-07 <ankitsuman07@gmail.com>
2024-05-06 19:02:57 +0530

    initial-state: initial state of jobs set

--- src/store/reducers/jobReducer.js
+++ src/store/reducers/jobReducer.js
@@ -1,3 +1,23 @@
-export default (state, action) => {
+const initialState = {
+    jobs: [
+        {
+            jdUid: '',
+            jdLink: '',
+            jobDetailsFromCompany: "",
+            maxJdSalary: 0,
+            minJdSalary: 0,
+            salaryCurrencyCode: '',
+            location: '',
+            minExp: 0,
+            maxExp: 0,
+            jobRole: '',
+            companyName: '',
+            logoUrl: ''
+        }
+    ]
+
+}
+
+export default (state = initialState, action) => {
     return state;
 }
