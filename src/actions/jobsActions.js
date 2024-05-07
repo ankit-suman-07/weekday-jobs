696f36d41c2c856b8d42a028164655f000fa1522
ankit-suman-07 <ankitsuman07@gmail.com>
2024-05-07 10:05:52 +0530

    api: api call implemented

--- src/actions/jobsActions.js
+++ src/actions/jobsActions.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import { setJobs, filterJobs } from '../store/jobsSlice';
 
-const jobsActions = () => {
-    return (
-        <div>jobsActions</div>
-    )
-}
+export const fetchJobs = () => async (dispatch) => {
+    try {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
 
-export default jobsActions
+        const body = JSON.stringify({
+            "limit": 10,
+            "offset": 0
+        });
+
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body
+        };
+
+        const response = await fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions);
+        const data = await response.json();
+
+        // Assuming result.jdList contains the array of jobs
+        dispatch(setJobs(data.jdList));
+    } catch (error) {
+        console.error('Error fetching jobs:', error);
+    }
+};
+
+export const applyFilters = (filters) => (dispatch) => {
+    dispatch(filterJobs(filters));
+};
+
+export const clearFilters = () => (dispatch) => {
+    // Dispatch an action to reset filters
+    dispatch(filterJobs({}));
+};
