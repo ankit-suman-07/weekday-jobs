37448b1fa26e2ae672f48e661fdfe74c73daf713
ankit-suman-07 <ankitsuman07@gmail.com>
2024-05-07 10:20:46 +0530

    feat: imported JobsPage in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import JobsPage from './components/JobsPage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         App
+        <JobsPage />
       </header>
     </div>
   );
