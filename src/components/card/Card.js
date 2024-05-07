4a22b1a58e6c3c3a4ee2605194a0d7828bc1b1b5
ankit-suman-07 <ankitsuman07@gmail.com>
2024-05-08 01:12:34 +0530

    feat: Card component completely structured

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -17,8 +17,47 @@ const Card = ({ job }) => {
                     <img src={TimeIcon} alt='hourglass icon' />
                     <span>Posted {Math.floor(Math.random() * 50) + 1} days ago.</span>
                 </div>
+
             </div>
-            {job.companyName}
+            <div className='card-company' >
+                <div className='card-company-logo' >
+                    <img src={job.logoUrl} alt='company icon' />
+                </div>
+                <div className='card-company-details' >
+                    <div className='company-name' >
+                        {job.companyName}
+                    </div>
+                    <div className='company-role' >
+                        {job.jobRole}
+                    </div>
+                    <div className='company-location' >
+                        {job.location}
+                    </div>
+                </div>
+            </div>
+            <div className='card-salary' >
+                <span>Estimated Salary: </span>
+                {
+                    job.minJdSalary
+                        ? <span>{job.minJdSalary} - {job.maxJdSalary} LPA</span>
+                        : <span>Upto {job.maxJdSalary} LPA</span>
+                }
+                <span></span>
+            </div>
+            <div className='about' >
+                <span>About Company:</span>
+                <span>About us</span>
+                <p>{job.jobDetailsFromCompany}</p>
+            </div>
+            <div className='card-experience' >
+                <span>Minimum Experience: </span>
+                <span>{job.minExp} years</span>
+            </div>
+            <button className='card-btn' >
+                <img src={LighteningIcon} alt='Lightning-icon' />
+                <span>Easy Apply</span>
+            </button>
+            <button className='card-btn' >Unlock Referral ads</button>
         </div>
     )
 }
